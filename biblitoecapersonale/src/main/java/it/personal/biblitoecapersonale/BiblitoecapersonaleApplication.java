56c98af8d61c6f1507f0fdf322bcb0c534eded1a
Pietro <pietr@Cicciopiero.station>
2025-03-12 21:15:47 +0100

    first commit

---
+++ biblitoecapersonale/src/main/java/it/personal/biblitoecapersonale/BiblitoecapersonaleApplication.java
@@ -0,0 +1,13 @@
+package it.personal.biblitoecapersonale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblitoecapersonaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblitoecapersonaleApplication.class, args);
+	}
+
+}
